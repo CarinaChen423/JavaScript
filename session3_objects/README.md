89800c275d2bd7b2dc03f1be30a194e71835a477
Zhe Chen <115776828+CarinaChen423@users.noreply.github.com>
2023-09-07 20:20:58 +0200

    Add files via upload

---
+++ session3_objects/README.md
@@ -0,0 +1,4 @@
+# Session3_Objects
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Chen2304/pen/xxmwZvj](https://codepen.io/Chen2304/pen/xxmwZvj).
+
---
+++ session3_objects/dist/script.js
@@ -0,0 +1,12 @@
+let xiaomi = {
+  name:"xiaomi",
+  age:3,
+  breed:"Domestic short hair cat",
+  friends:["Fa Cai","Lucky"],
+  address:{
+    street : "Tasmowa 2",
+    city : "Warsaw",
+    numbers : 12648
+  }
+};
+console.log(xiaomi.friends[0]);
