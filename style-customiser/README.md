4616e9780446fa8127f55aa3f92eaaa6d727479d
Zhe Chen <115776828+CarinaChen423@users.noreply.github.com>
2023-09-16 00:18:00 +0200

    Add files via upload

---
+++ style-customiser/README.md
@@ -0,0 +1,4 @@
+# Style Customiser
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/JamesHolcombe/pen/poqrjXY](https://codepen.io/JamesHolcombe/pen/poqrjXY).
+
---
+++ style-customiser/dist/script.js
@@ -0,0 +1,16 @@
+function applyBackgroundColor() {
+  const selectedBgColor = document.getElementById("bg-color").value;
+  document.body.style.backgroundColor = selectedBgColor;
+}
+
+function applyFontFamily() {
+  const selectedFont = document.getElementById("font-family").value;
+  document.body.style.fontFamily = selectedFont;
+}
+
+function applyFontColor() {
+ 
+  const selectedColor = 
+document.getElementById("font-color").value;
+  document.body.style.color = selectedColor;
+}
